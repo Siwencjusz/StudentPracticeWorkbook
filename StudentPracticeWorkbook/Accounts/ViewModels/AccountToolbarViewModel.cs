8b18c6b862fd2afbb4cc78e6b177e1de9cfe351c
Łukasz Mierzejewski <Lukasz.Mierzejewski@billennium.pl>
2016-11-18 12:34:32 +0100

    New content

--- StudentPracticeWorkbook/Accounts/ViewModels/AccountToolbarViewModel.cs
+++ StudentPracticeWorkbook/Accounts/ViewModels/AccountToolbarViewModel.cs
@@ -1,23 +1,10 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Windows;
-using System.Windows.Data;
 using System.Windows.Input;
 using Accounts.Views;
-using Admin.Views;
-using Company;
-using Company.Views;
 using Prism.Events;
 using Prism.Modularity;
 using Prism.Regions;
-using Student;
-using Student.Views;
-using Supervisor;
-using Supervisor.Views;
 using Workbook.Commons;
 using Workbook.Commons.Events;
 
@@ -27,14 +14,12 @@ namespace Accounts.ViewModels
     {
         private readonly RegionManager _regionManager;
         private readonly LoggedUserService _loggedUserService;
-        private readonly ModuleManager _moduleManager;
         private readonly EventAggregator _eventAggregator;
 
-        public AccountToolbarViewModel(RegionManager regionManager, LoggedUserService loggedUserService, ModuleManager moduleManager, EventAggregator eventAggregator)
+        public AccountToolbarViewModel(RegionManager regionManager, LoggedUserService loggedUserService, EventAggregator eventAggregator)
         {
             _regionManager = regionManager;
             _loggedUserService = loggedUserService;
-            _moduleManager = moduleManager;
             _eventAggregator = eventAggregator;
 
             _eventAggregator.GetEvent<LoginEvent>().Subscribe(OnLogin);
@@ -50,7 +35,7 @@ namespace Accounts.ViewModels
         {
             get
             {
-                    return _loggedUserService.ActiveUser.Name + " " + _loggedUserService.ActiveUser.LastName;
+                    return "";
             }
         }
         public bool UserIsLogged
--- StudentPracticeWorkbook/Company/ViewModels/CompanyDepartmentsViewModel.cs
+++ StudentPracticeWorkbook/Company/ViewModels/CompanyDepartmentsViewModel.cs
@@ -2,15 +2,23 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using Workbook.BLL.Services.Serv;
+using Workbook.DAL.Entities;
 
 namespace Company.ViewModels
 {
     public class CompanyDepartmentsViewModel : BindableBase
     {
-        public CompanyDepartmentsViewModel()
-        {
+        private readonly DepartmentService _departmentService;
 
+        public CompanyDepartmentsViewModel(DepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+            DepartmentsList = new ObservableCollection<Department>(_departmentService.FindAll());
         }
+
+        public ObservableCollection<Department> DepartmentsList { get; set; }
     }
 }
